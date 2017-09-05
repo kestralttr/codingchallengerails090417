2f3625202e0593e7a1aa49aa78bb9942a9f19161
Alex Bennett <kestralttr@gmail.com>
2017-09-05 01:05:14 -0700

    successfully sends SMS via Rails

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,4 +11,35 @@ class ContactsController < ApplicationController
 
   end
 
+  def create
+    @api_token = ENV["APITOKEN"]
+
+    puts params
+
+    id = params[:id]
+
+    data = {
+      body: {
+        "recipients": [
+          "contact-" + id
+        ],
+        "message": {
+          "body": "This SMS message will be sent immediately"
+        }
+      }
+    }
+
+    api_request_string = "https://stage.skipio.com/api/v2/messages?token=" + @api_token
+    @response = HTTParty.post(api_request_string, data)
+
+    puts "incoming response code:"
+    puts @response.code
+
+    if @response.code == 201
+      render plain: "Request Successful"
+    else
+      render plain: "Error!"
+    end
+  end
+
 end
