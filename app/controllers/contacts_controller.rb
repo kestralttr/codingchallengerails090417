a1a59d38685eeb3454a3d98337a2da5fdd37c49b
Alex Bennett <kestralttr@gmail.com>
2017-09-04 22:36:52 -0700

    adjusts index.html api request

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,11 +2,11 @@ class ContactsController < ApplicationController
 
   def index
 
-    api_request_string = ENV['APIURL']
+    api_request_string = "https://stage.skipio.com/api/v2/contacts?token=" + ENV['APITOKEN'] + "&page=1"
     @response = HTTParty.get(api_request_string)
 
-    puts 'API URL incoming:'
-    puts api_request_string
+    # puts 'API URL incoming:'
+    puts @response['data'][0]
 
   end
 
