a2b4fba6fb26192751fe033d6cace3858aa509ee
Alex Bennett <kestralttr@gmail.com>
2017-09-06 00:13:20 -0700

    removes console logs

--- app/controllers/contacts_controller/contacts_controller.rb
+++ app/controllers/contacts_controller/contacts_controller.rb
@@ -5,17 +5,12 @@ class ContactsController < ApplicationController
     request_object = SkipioRequest.new()
     @response = request_object.get_contacts()
 
-    puts @response['data'][0]
-
   end
 
   def create
 
     request_object = SkipioRequest.new()
     @response = request_object.send_message(params[:id],params[:message])
-
-    puts "incoming response code:"
-    puts @response.code
 
     if @response.code == 201
       render plain: "Request Successful"
